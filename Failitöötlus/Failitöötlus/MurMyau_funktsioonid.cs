4b0aa66478a6278b6b2369ffef234af60865dfde
Andrii611 <ffgggfegujd@gmail.com>
2026-03-11 11:11:39 +0200

    Refactor functions and update file handling

--- Failitöötlus/Failitöötlus/MurMyau_funktsioonid.cs
+++ Failitöötlus/Failitöötlus/MurMyau_funktsioonid.cs
@@ -1,34 +1,35 @@
-﻿﻿using System;
-using System.Collections;
+﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.IO;
+using System.Linq;
 using System.Text;
-namespace osa4ülesanded
+using System.Threading.Tasks;
+
+namespace Naidis_C.OsaUlesanded.Osa4
 {
-    public class funktsioonid
+    internal class Ulesanded
     {
-        // 1
-        public static void lemmiktoiduSalvestamine()
+        public static void retseptid()
         {
             try
             {
+
                 string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Retseptid.txt");
-                StreamWriter retsept = new StreamWriter(path, true);
-                Console.WriteLine("Sisesta üks Itaalia toidu nimi");
+                StreamWriter text = new StreamWriter(path, true);
+                Console.WriteLine("Sisesta Itaalia toidu nime");
                 string lause = Console.ReadLine();
-                retsept.WriteLine(lause);
-                retsept.Close();
+                text.WriteLine(lause);
+                text.Close();
+
+
+
             }
             catch (Exception)
             {
-                Console.WriteLine("Viga failiga!");
+                Console.WriteLine("Mingi viga failiga");
             }
 
         }
-        // 2
-        public static void menuuKuvamine()
+        public static void retseptilistiavamine()
         {
             try
             {
@@ -37,54 +38,103 @@ namespace osa4ülesanded
                 string laused = text.ReadToEnd();
                 text.Close();
                 Console.WriteLine(laused);
+
             }
             catch (Exception)
             {
                 Console.WriteLine("Mingi viga failiga, ei saa faili lugeda");
             }
         }
-
-        // 3
-        public static void KoostisosadeMuutmine()
+        public static void KoostisosadFail()
         {
             try
             {
-                string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Koostisosad.txt");
+                string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "KoostisOsad.txt");
+                StreamWriter text = new StreamWriter(path, true);
+                Console.WriteLine("Sisesta koostis osad");
+                string lause = Console.ReadLine();
+                text.WriteLine(lause);
+                text.Close();
+
+
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Mingi viga failiga");
+            }
+        }
+        public static void KoostisOsadelugemine()
+        {
+            try
+            {
+                string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "KoostisOsad.txt");
+                StreamReader text = new StreamReader(path);
+                string laused = text.ReadToEnd();
+                text.Close();
+                Console.WriteLine(laused);
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Mingi viga failiga, ei saa faili lugeda");
+            }
+        }
+        public static List<string> Koostisosadelist(string fail)
+        {
+            List<string> KoostisOsade_list = new List<string>();
+            try
+            {
+                string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "KoostisOsad.txt");
                 foreach (string rida in File.ReadAllLines(path))
                 {
-                    List<string> koostisosadlist = new List<string>();
-                    string[] koostisosad = rida.Split(", ");
-
-                    foreach (string i in koostisosad)
-                    {
-                        koostisosadlist.Add(i);
-                    }
-
-                    if (koostisosadlist.Count > 0)
-                        koostisosadlist[0] = "Kvaliteetne oliiviõli";
-
-                    for (int i = koostisosadlist.Count - 1; i >= 0; i--)
-                    {
-                        if (koostisosadlist[i].ToLower() == "ketšup")
-                        {
-                            koostisosadlist.RemoveAt(i);
-                            Console.WriteLine("Kustutasime ketšupi koostisosast!");
-                        }
-                    }
-                    foreach (string koostiosa in koostisosadlist)
-                        Console.WriteLine(koostiosa);
+                    KoostisOsade_list.Add(rida);
                 }
             }
-            catch (Exception e)
+            catch (Exception)
             {
-                Console.WriteLine($"Viga! {e}");
+                Console.WriteLine("Viga failiga");
             }
-
+            return KoostisOsade_list;
         }
 
-        public static void otsingListist()
+        public static void Koostisosademuutmine(string fail)
         {
-            
+            List<string> KoostisOsade_List = Koostisosadelist(fail);
+
+            foreach (string KoostisOsad in KoostisOsade_List)
+            {
+                Console.WriteLine(KoostisOsad);
+            }
+            KoostisOsade_List.Remove("Ketšup");
+
+            if (KoostisOsade_List.Count > 1)
+                KoostisOsade_List[1] = "Kvaliteetne Oliiviõli";
+
+            Console.WriteLine(" kustutasime Ketšup");
+            foreach (string KoostisOsad in KoostisOsade_List)
+            {
+                Console.WriteLine(KoostisOsad);
+            }
+        }
+        public static void Otsing(string file)
+        {
+            List<string> KoostisOsade_List = Koostisosadelist(file);
+            Console.WriteLine("Sisesta Koostisosa nimi, mida otsida:");
+            string otsitav = Console.ReadLine();
+
+            if (KoostisOsade_List.Contains(otsitav))
+                Console.WriteLine("KoostisOsa " + otsitav + " on olemas.");
+            else
+                Console.WriteLine("Sellist KoostisOsa pole.");
+        }
+        public static void Koostisosadesalvestamine(string file)
+        {
+            List<string> KoostisOsade_List = Koostisosadelist(file);
+            string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Kuud.txt");
+            File.WriteAllLines(path, KoostisOsade_List);
+            Console.WriteLine("Koostis osad on salvestatud.");
+
         }
     }
 }
