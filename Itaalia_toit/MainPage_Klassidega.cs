210949a50c3218a2067f663a0bab57736c3d1655
Opilane TTHK <opilane@tthk.ee>
2026-03-11 15:05:45 +0200

    Itaalia_Toit

---
+++ Itaalia_toit/MainPage_Klassidega.cs
@@ -0,0 +1,22 @@
+﻿namespace Itaalia_toit
+{
+    public class MainPage_klassidega
+    {
+        public static void Main(string[] args)
+        {
+            bool tootab = true;
+            {
+                Console.WriteLine("Funktsioonid");
+                string valik = Console.ReadLine();
+                switch (valik)
+                {
+                    case "1": Itaalia_funktsioonid_klassidega.LaeAndmedFailist(); break;
+                    case "2": Itaalia_funktsioonid_klassidega.ItaaliaRestoran(); break;
+                    case "3":
+                        Itaalia_funktsioonid_klassidega.LiisaUusToit(); break;
+                }
+            }
+
+        }
+    }
+}
