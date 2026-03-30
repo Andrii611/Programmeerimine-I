dc150751e04e215a13c1a741bccc0a3c419f88d8
Andrii611 <ffgggfegujd@gmail.com>
2026-03-30 10:59:40 +0300

    Refactor StartPage class and add functionality

--- StartPage_osa4.cs
+++ StartPage_osa4.cs
@@ -1,20 +1,52 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
-namespace Naidis_IKTpv25
+namespace Naidis_csharp
 {
-<<<<<<< HEAD
-    public static void Main(string[] args)
-    {
+    internal class start_page_Osa4cs
 
-=======
-    internal class StartPage_osa4
     {
         public static void Main(string[] args)
         {
 
+            while (true)
+            {
+                Console.WriteLine("4. Osa Funktsioonid");
+                string valik = Console.ReadLine();
+                string fail = "";
+                List<string> list= new List<string>();
+                switch (valik)
+                {
+                    case "1": Osa4_funktsioonid.Faili_kirjutamine(); break;
+                    case "2": Osa4_funktsioonid.Faili_lugemine(); break;
+                       
+                    case "3":
+                        Console.Write("failinimi: ");
+                        fail = Console.ReadLine();
+                        Osa4_funktsioonid.Ridade_lugemine(fail);
+                  
+
+                        break;
+                    case "4":
+                        Console.Write("failinimi: ");
+                        fail = Console.ReadLine();
+                        list =  Osa4_funktsioonid.Ridade_lugemine_listiks(fail);
+                        break;
+
+                    case "5": Osa4_funktsioonid.Listi_muutmine_kuvamine(); break;
+                    case "6": Osa4_funktsioonid.Otsing_nimekirjast(); break;
+                    case "7":Osa4_funktsioonid.Listi_salvestamine(); break;
+                        
+
+
+
+                        break;
+                    default: Console.Write("sisesta kehtiv number: "); break;
+                }
+            }
         }
->>>>>>> a6ef45118bf66ef5fd4bc7e4d201537049eba000
     }
 }
